af07c6808c1a4365d3c515786d75075a7b9630ac
user <user@Test.Dlink>
2020-11-06 00:35:21 +0700

    добавил игру

---
+++ Assets/Genome.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Genome
+{
+    public static int skillCount = 5;
+    //веса - это веса для скалярного произведения (для нейронной сети)
+    public float[] weights;
+    //скилы - это кормление, атака, защита и размер (может быть от 0 до 4)
+    public int[] skills;
+
+    public Genome(int size)
+    {
+        weights = new float[size];
+        skills = new int[skillCount];
+        for(int i = 0; i < size; i++)
+        {
+            weights[i] = UnityEngine.Random.Range(-1f, 1f);
+        }
+    }
+
+    public Genome(Genome a)
+    {
+        weights = new float[a.weights.Length];
+        Array.Copy(a.weights, 0, weights, 0, a.weights.Length);
+        skills = new int[skillCount];
+        Array.Copy(a.skills, 0, skills, 0, skillCount);
+    }
+    //если установить жесткие веса - при весе 1 - микробы сильно кучкуются; при 0 - микробы стараются быть поодиночке, при - 0.5 еще больше разделены и двигаются медленно, при 0.5 кучкуются
+    public void Mutate(float value)
+    {
+        for(int i = 0; i < weights.Length; i++)
+        {
+            if (UnityEngine.Random.value < 0.1)
+                weights[i] += UnityEngine.Random.Range(-value, value);
+               // weights[i] = 0.5f ;
+        }
+        for(int i = 0; i < skillCount; i++)
+        {
+            if(UnityEngine.Random.value < 0.05) {
+                skills[i] = UnityEngine.Random.Range(0, 4);
+            }
+        }
+    }
+    
+}
---
+++ Assets/MainController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public Vector2 area = new Vector2(10f, 10f);
+
+    public GameObject bacteriumPrefab;
+    public GameObject boidPrefab;
+    public GameObject foodPrefab;
+
+    private int frame = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartFood();
+        Evolution();
+        //Boids();
+    }
+    //другой вид бактерий (эксперимент) - не едят друг друга но двигаются стаей
+    private void Boids()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject b = Instantiate(boidPrefab, new Vector3(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y), 0), Quaternion.identity);
+        }
+    }
+    //for bacteriums start value was 100
+    private void Evolution()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Genome genome = new Genome(64);
+            GameObject b = Instantiate(bacteriumPrefab, new Vector3(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y), 0), Quaternion.identity);
+            b.name = "bacterium";
+            b.GetComponent<AI>().Init(genome);
+        }
+        
+    }
+
+     private void StartFood ()
+    {
+        for (int i = 0; i < 2000; i++)
+        {
+            GetFood();
+        }
+    }
+
+    private void GetFood ()
+    {   
+        GameObject food = Instantiate(foodPrefab, new Vector3(Random.Range(-area.x, area.x), Random.Range(-area.y, area.y), 0), Quaternion.identity);
+            food.name = "food";
+    }
+
+    void FixedUpdate()
+    {
+        if(frame % 1 == 0)
+        {
+            GetFood();
+            GetFood();
+        }
+        frame++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
