11caeff4c5e3d4a884b2eb43a17b2482e42d960f
user <user@Test.Dlink>
2020-11-23 01:04:28 +0700

    добавил интерфейс

---
+++ Assets/IMove.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMove 
+{
+    void RotateEnemy();
+    void FindGoalToMove();
+    void Move();
+
+}
